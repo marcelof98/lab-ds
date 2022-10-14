84720450d42a74d870ae21be5ffac79c827978e4
Marcelo Cardoso <marcelofillipe98@gmail.com>
2022-10-14 19:33:18 -0300

    feat: SignIn page

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,8 @@
-import { Envelope, Lock } from 'phosphor-react';
-import { Button } from './components/Button';
-import { Checkbox } from './components/Checkbox';
-import { Heading } from './components/Heading';
-import { Text } from './components/Text';
-import { TextInput } from './components/TextInput';
-import { Logo } from './Logo'
+import { SignIn } from './pages/SignIn';
 import './styles/global.css';
 
 export function App() {
   return (
-    <div className="w-screen h-screen bg-gray-900 flex flex-col justify-center items-center text-gray-100">
-      <header className="flex flex-col items-center">
-        <Logo />
-        <Heading size="md">
-          Ignite Lab
-        </Heading>
-
-        <Text size="md" className="text-gray-400 leading-none">Faça login e comece a usar!</Text>
-      </header>
-      <form className="flex flex-col gap-4 items-stretch w-full max-w-sm mt-8">
-        <label htmlFor="email" className="flex flex-col gap-1">
-          <Text className="font-semibold" size="sm">Endereço de e-mail</Text>
-          <TextInput.Root>
-            <TextInput.Icon>
-              <Envelope />
-            </TextInput.Icon>
-            <TextInput.Input id="email" type="mail" placeholder="meuemail@exemplo.com" />
-          </TextInput.Root>
-        </label>
-
-        <label htmlFor="email" className="flex flex-col gap-1">
-          <Text className="font-semibold" size="sm">Senha</Text>
-          <TextInput.Root>
-            <TextInput.Icon>
-              <Lock />
-            </TextInput.Icon>
-            <TextInput.Input id="password" type="password" placeholder="*************" />
-          </TextInput.Root>
-        </label>
-
-        <label htmlFor="remember" className="flex items-center gap-2">
-          <Checkbox id="remember" />
-          <Text size="xs" className="text-gray-200">Lembrar-me por 30 dias</Text>
-        </label>
-
-        <Button type="submit" className="mt-4">Entrar na plataforma</Button>
-      </form>
-
-      <footer className="flex flex-col items-center gap-1 mt-6">
-        <Text asChild size="xs">
-          <a href="" className="text-gray-400 underline">Esqueceu sua senha?</a>
-        </Text>
-
-        <Text asChild size="xs">
-          <a href="" className="text-gray-400 underline">Não possui conta? Crie uma agora!</a>
-        </Text>
-      </footer>
-    </div >
+    <SignIn />
   )
 }
