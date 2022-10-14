2f9b3260cfe911b2ee63ed74737326a64eaf0d85
Marcelo Cardoso <marcelofillipe98@gmail.com>
2022-10-14 15:52:11 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-gray-900">Hello world</h1>
+      <button className="text-gray-900 bg-cyan-500 hover:bg-cyan-300 px-4 py-2 rounded font-medium">Enviar</button>
+    </>
+  )
+}
